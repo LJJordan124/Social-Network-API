a623addf420640eea36f3ee97f5bbd8350e5373e
LJJordan124 <jordan3313.lj@gmail.com>
2023-08-17 13:28:47 -0400

    server update

--- server.js
+++ server.js
@@ -8,3 +8,15 @@ const routes = require('./routes');
 //Defined port for the server to listen to.
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+//Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(routes);
+
+//Once the connection is open, start the server and listen on the port specifed.
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+  });
+});
