5b221d3aef5ef09cc1ffea3a096fc8a336bec3fa
LJJordan124 <jordan3313.lj@gmail.com>
2023-08-17 22:44:42 -0400

    Reaction model

---
+++ models/reaction.js
@@ -0,0 +1,34 @@
+//Import required models from the 'mongoose' library.
+const { Schema, Types } = require('mongoose');
+
+//Schema to create Reaction model.
+const reactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      maxlength: 280,
+    },
+    userName: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+//Export the 'reactionSchema' so it can be used.
+module.exports = reactionSchema;
